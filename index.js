215687ec49c91e61a43e6d6a27575475e8c047a4
mostafaljamal85 <mostafa.aljamal@st.ul.edu.lb>
2023-03-30 09:34:28 +0200

    test token enable

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ fetch('https://testpublishapi.onrender.com/')
       .then(response => response.json())
       .then(json => {
         var _employee=document.getElementById("employee")
+        
       for (let i = 0; i < json.length; i++) {
         var _div=document.createElement("div")
-            _div.className= "swiper-slide";
-            _div.innerHTML=`
-        <div class="testimonial-item">
-          <img src=${json[i].image} class="testimonial-img" alt="">
-          <h3>${json[i].firstname}</h3>
-          <h4>${json[i].department}</h4>
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-           labore illum veniam.
-            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-          </p>
-        </div>
-      `
-      _employee.appendChild(_div)
+        _div.className= "swiper-slide";
+        _div.style.width="100px"
+      var content=`
+      <div class="testimonial-item">
+        <img src=${json[i].image} class="testimonial-img" alt="">
+        <h3>${json[i].firstname}</h3>
+        <h4>${json[i].department}</h4>
+        <p>
+          <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+         labore illum veniam.
+          <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+        </p>
+      </div>
+    `
+            _div.innerHTML=content
+            _employee.appendChild(_div)
+
       }
+    console.log(_employee)
       })
--- team.js
+++ team.js
@@ -1,27 +1,35 @@
+var _team=document.getElementById("newteam")
+
 fetch('https://testpublishapi.onrender.com/')
       .then(response => response.json())
       .then(json => {
-        var _team=document.getElementById("newteam")
+       
         for (let i = 4; i <8; i++) {
             var _div1=document.createElement("div")
-            _div1.className="col-lg-3 col-md-6 d-flex align-items-stretch"
-            _div1.innerHTML=`
-                <div class="member">
-                <div class="member-img">
-                    <img src=${json[i].image} class="img-fluid" alt="${json[i].alt}">
-                    <div class="social">
-                    <a href=""><i class="bi bi-twitter"></i></a>
-                    <a href=""><i class="bi bi-facebook"></i></a>
-                    <a href=""><i class="bi bi-instagram"></i></a>
-                    <a href=""><i class="bi bi-linkedin"></i></a>
-                    </div>
+    _div1.className="col-lg-3 col-md-6 d-flex align-items-stretch"
+             var content=` <div class="member">
+            <div class="member-img">
+                <img src=${json[i].image} class="img-fluid" alt="${json[i].alt}">
+                <div class="social">
+                <a href=""><i class="bi bi-twitter"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
                 </div>
-                <div class="member-info">
-                    <h4>${json[i].firstname}</h4>
-                    <span>${json[i].department}</span>
-                </div>
-            </div>`
-          _team.appendChild(_div1)
+            </div>
+            <div class="member-info">
+                <h4>${json[i].firstname}</h4>
+                <span>${json[i].department}</span>
+            </div>
+        </div>`
+            _div1.innerHTML=content
+            // console.log(_div1)
+            _team.appendChild(_div1)
+
         }
+
+        // console.log("after loop")
+        // console.log(_team)
+
               })
       
